11a9b140d4acffce69ac768ec2b159684bc81bd1
Rubén Ibáñez Carmona <ruben.ibc@gmail.com>
2016-03-09 10:39:04 +0100

    Adding the first koans I did, also the second ones. The current will be allways the non numbered.

---
+++ first_time_koans/triangle.rb
@@ -0,0 +1,39 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a, b, c)
+  # WRITE THIS CODE
+  # if a == b || a == c || b == c
+  #   if a == b && a == c
+  #     return :equilateral
+  #   end
+  #   return :isosceles
+  # else
+  #   return :scalene
+  # end
+
+  raise TriangleError, "Illegal triangle: [#{a}, #{b}, #{c}]" if
+    [a, b, c].max >= (a + b + c) / 2.0
+
+  return :equilateral if a == b and b == c
+  return :isosceles if a == b or b == c or a == c
+  return :scalene
+  
+  
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
