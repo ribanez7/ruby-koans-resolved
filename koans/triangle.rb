08c0622615c376f0c61a63263c04bdc627976fa9
Rubén Ibáñez Carmona <ruben.ibc@gmail.com>
2016-04-04 13:07:17 +0200

    exceptions done, again triangle now

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,9 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  return :equilateral if a == b and b == c
+  return :isosceles if a == b or a == c or b == c
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
